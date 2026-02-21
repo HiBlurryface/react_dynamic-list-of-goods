2559401441c9364fa8be643529dc2a26bf972de1
Alexandr <AlexandrKsenz@gmail.com>
2026-02-21 21:57:27 +0100

    Solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,17 +4,23 @@ import { Good } from '../types/Good';
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+  return fetch(API_URL).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Failed to load goods' + response.status);
+    }
+  })
 }
 
 export const get5First = () => {
   return getAll().then(goods => {
-    return goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5);
+    return [...goods].sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5);
   });
 };
 
 export const getRedGoods = () => {
   return getAll().then(goods => {
-    return goods.filter(item => item.color === 'red');
+    return [...goods].filter(item => item.color === 'red');
   });
 };

cf33c36203020f39c6755af3ee648d5468a54e0d
Alexandr <AlexandrKsenz@gmail.com>
2026-02-21 21:38:51 +0100

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[] | null>(null);
 
-export const App: React.FC = () => (
-  <div className="App">
+  const loadData = async (sortBy: string) => {
+    let data;
+
+    if (sortBy === 'getFiveFirst') {
+      data = await get5First();
+    } else if (sortBy === 'getRed') {
+      data = await getRedGoods();
+    } else {
+      data = await getAll();
+    }
+
+    setGoods(data);
+  }
+
+  return <div className="App">
     <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="all-button">
+    <button type="button" data-cy="all-button" onClick={() => loadData('getAll')}>
       Load all goods
     </button>
 
-    <button type="button" data-cy="first-five-button">
+    <button type="button" data-cy="first-five-button" onClick={() => loadData('getFiveFirst')}>
       Load 5 first goods
     </button>
 
-    <button type="button" data-cy="red-button">
+    <button type="button" data-cy="red-button" onClick={() => loadData('getRed')}>
       Load red goods
     </button>
 
-    <GoodsList goods={[]} />
+    {goods && <GoodsList goods={goods} />}
   </div>
-);
+};
